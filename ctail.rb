848999650664371275f1ae952c4c3ad7ed124af3
Nat Welch <nat@natwelch.com>
2011-04-11 01:13:30 -0700

    Some random connection error.

--- ctail.rb
+++ ctail.rb
@@ -24,11 +24,11 @@ req.basic_auth(user, password)
 res = http.start{|h| h.request(req) }
 
 groups = {}
-topics = {}
 
-JSON.parse(res.body)["groups"].each {|group| groups[group["id"]] = group["name"] }
+JSON.parse(res.body)["groups"].each {|group| groups[group["id"]] = {:name => group["name"] }}
 
-groups.each {|id|
+groups.each_pair {|id, group|
+   topics = {}
    url = URI.parse(TOPICS_URL % id)
    http = Net::HTTP.new(url.host, url.port)
    http.use_ssl = true
@@ -41,4 +41,25 @@ groups.each {|id|
          :url => topic["url"]
       }
    }
+
+   group[:topics] = topics
 }
+
+groups.each do |id, group|
+   group[:topics].each_pair do |id, topic|
+      url = URI.parse(TOPIC_URL % id)
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true
+      req = Net::HTTP::Get.new(url.path)
+      req.basic_auth(user, password)
+      req.set_form_data({ :mark_read => false })
+      res = http.start{|h| h.request(req) }
+      json = JSON.parse(res.body)
+
+      puts "\nGroup: #{group[:name]}"
+      puts "\t #{topic[:name]}"
+      json.each {|msg|
+         puts "\t\t#{msg[:user][:username]}: #{msg[:message]}" if !msg.nil?
+      }
+   end
+end
